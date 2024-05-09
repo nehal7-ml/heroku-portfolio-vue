63cd129d4d06dc3a222c2d61b880f0cddd3b127f
nehal7-ml <nehal.sk.99@gmail.com>
2024-05-09 22:51:53 +0530

    fix deployment issue

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ import express from 'express'
 import serveStatic from 'serve-static'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import {config} from 'dotenv'
+config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+console.log(import.meta.url);
 const app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 const port = process.env.PORT || 80
-app.listen(port)
+app.listen(port, () => console.log(`App listening on port ${port}`))
